b8131b57cee76527aca9f7c94af2f92010c1cf95
Nobuyuki Aoki <http://www.google.com/recaptcha/mailhide/d?k=01Dzf3dV0lth_vnOrUfGePbw==&c=wglleHFIzA1ROWYFf_vtt6j1pchskwiKPWnHGaKnrek=>
2015-02-12 14:28:05 +0900

    fix glycosequence

--- src/main/java/org/glycoinfo/batch/search/WurcsMotifSearchBatch.java
+++ src/main/java/org/glycoinfo/batch/search/WurcsMotifSearchBatch.java
@@ -13,6 +13,7 @@ import org.glycoinfo.rdf.SelectSparql;
 import org.glycoinfo.rdf.dao.SparqlDAO;
 import org.glycoinfo.rdf.dao.SparqlDAOSesameImpl;
 import org.glycoinfo.rdf.dao.SparqlEntity;
+import org.glycoinfo.rdf.glycan.GlycoSequence;
 import org.glycoinfo.rdf.glycan.MotifInsertSparql;
 import org.glycoinfo.rdf.glycan.MotifSequenceSelectSparql;
 import org.glycoinfo.rdf.utils.TripleStoreProperties;
@@ -66,7 +67,9 @@ public class WurcsMotifSearchBatch {
 	}
 
 	SearchSparql getSearchSparql() {
-		return new SearchSparqlBean();
+		SearchSparqlBean ssb = new SearchSparqlBean();
+		ssb.setGlycoSequenceVariable(GlycoSequence.URI);
+		return ssb;
 	}
 	
 	@Bean
