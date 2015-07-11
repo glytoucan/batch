4364627f183a9fa839281671f0ff709624bb90e9
Nobuyuki Paul Aoki <aokinobu@gmail.com>
2015-07-11 23:33:27 +0900

    fix script

--- runKcf.sh
+++ runKcf.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=128M -Djava.security.egd=file:/dev/./urandom"
-mvn -U -DskipTests=true -P wurcs spring-boot:run
+mvn -U -DskipTests=true -P kcf spring-boot:run
