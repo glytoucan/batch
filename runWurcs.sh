1fc091ef86109b053393194daf30b7ce6f2c926a
aoki <aoki@bluetree.jp>
2016-06-16 17:48:26 +0900

    scripts

--- runWurcs.sh
+++ runWurcs.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=128M -Djava.security.egd=file:/dev/./urandom"
-mvn -U -DskipTests=true -P wurcs spring-boot:run
+. ./triplestore.env
+echo SPRING_TRIPLESTORE_URL=$SPRING_TRIPLESTORE_URL SPRING_TRIPLESTORE_PASSWORD=$SPRING_TRIPLESTORE_PASSWORD mvn -U -DskipTests=true -P wurcs spring-boot:run
+SPRING_TRIPLESTORE_URL=$SPRING_TRIPLESTORE_URL SPRING_TRIPLESTORE_PASSWORD=$SPRING_TRIPLESTORE_PASSWORD mvn -U -DskipTests=true -P wurcs spring-boot:run
