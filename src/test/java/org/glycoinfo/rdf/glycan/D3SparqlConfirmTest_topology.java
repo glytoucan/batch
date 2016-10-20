1134488e80b1a1a1361661607008c84ae182df8d
徳永敬子 <keiko.t914@gmail.com>
2016-10-20 16:33:26 +0900

    GRAB graph sprawl

--- src/test/java/org/glycoinfo/rdf/glycan/D3SparqlConfirmTest_topology.java
+++ src/test/java/org/glycoinfo/rdf/glycan/D3SparqlConfirmTest_topology.java
@@ -24,20 +24,35 @@ public class D3SparqlConfirmTest_topology {
 	@Test
 	public void jsonSerializationTest() throws Exception {
 	    D3SequenceSelectSparql_has_topology dp = new D3SequenceSelectSparql_has_topology(); 
-//	    D3SeqenceSelectSparql_topology_by dpb = new D3SeqenceSelectSparql_topology_by();
+	    D3SequenceSelectSparql_topology_by dpb = new D3SequenceSelectSparql_topology_by();
+	    GRABSequenceSelectSparql_subsumedby gdp = new GRABSequenceSelectSparql_subsumedby();
+	    GRABSequenceSelectSparql_subsumes gdpb = new GRABSequenceSelectSparql_subsumes();
 	    
 	    SparqlEntity sparqlentity_1 = new SparqlEntity();
-//	    SparqlEntity sparqlentity_2 = new SparqlEntity();
+	    SparqlEntity sparqlentity_2 = new SparqlEntity();
+	    SparqlEntity sparqlentity_3 = new SparqlEntity();
+	    SparqlEntity sparqlentity_4 = new SparqlEntity();
 	    
 //	    sparqlentity.setValue(Saccharide.PrimaryId, "G000TEST");
-	    sparqlentity_1.setValue(Glycosidic_topology.PrimaryId_1, "GxxxxxxD");
+	    sparqlentity_1.setValue(Glycosidic_topology.PrimaryId_1, "G63977XF");
 	    dp.setSparqlEntity(sparqlentity_1);
 	    
-//	    sparqlentity_2.setValue(Glycosidic_topology.PrimaryId_2, "GxxxxxxD");
-//	    dp.setSparqlEntity(sparqlentity_2);
+	    sparqlentity_2.setValue(Glycosidic_topology.PrimaryId_2, "G63977XF");
+	    dpb.setSparqlEntity(sparqlentity_2);
+	    
+	    sparqlentity_3.setValue(Glycosidic_topology.PrimaryId_1, "G63977XF");
+	    gdpb.setSparqlEntity(sparqlentity_1);
+	    
+	    sparqlentity_4.setValue(Glycosidic_topology.PrimaryId_2, "G63977XF");
+	    gdp.setSparqlEntity(sparqlentity_2);
 
 	     logger.debug(dp.getSparql());
-//	     logger.debug(dpb.getSparql());
+	     logger.debug("\n");
+	     logger.debug(dpb.getSparql());
+	     logger.debug("\n");
+	     logger.debug(gdp.getSparql());
+	     logger.debug("\n");
+	     logger.debug(gdpb.getSparql());
 	     
 //	     ObjectMapper mapper = new ObjectMapper();
 //	     // {"glycanId":0,"accessionNumber":"G001234","dateEntered":"1970-01-01T00:00:00Z","structure":"structureString","structureLength":null,"mass":123.0,"motifs":null,"contributor":{"userId":1,"userName":"aoki","loginId":"aokinobu","email":"support@glytoucan.org","active":true,"validated":false,"affiliation":"Soka University","dateRegistered":"1970-01-01T00:00:00Z","lastLoggedIn":"1970-01-01T00:00:00Z","roles":null}}
