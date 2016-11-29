9f0ef0a2430e065ab6ace1f41e38669c95d940b1
daisuke shinmachi <sinmachi@soka.ac.jp>
2016-11-29 19:13:48 +0900

    modified DeleteInsertSparqlBean

--- src/main/java/org/glycoinfo/rdf/DeleteInsertSparqlBean.java
+++ src/main/java/org/glycoinfo/rdf/DeleteInsertSparqlBean.java
@@ -24,58 +24,132 @@ public class DeleteInsertSparqlBean implements DeleteInsertSparql {
 	public DeleteInsertSparqlBean() {
 	}
 
+	// PREFIX
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+	// DELETE
 	@Override
 	public String getDelete() throws SparqlException {
-		// TODO Auto-generated method stub
-		return null;
+		return delete;
 	}
 
 	@Override
-	public void setDelete(String insert) {
-		// TODO Auto-generated method stub
-
+	public void setDelete(String delete) {
+		this.delete = delete;
 	}
 
+	// INSERT
 	@Override
 	public String getInsert() throws SparqlException {
-		// TODO Auto-generated method stub
-		return null;
+		return insert;
 	}
 
 	@Override
 	public void setInsert(String insert) {
-		// TODO Auto-generated method stub
-
+		this.insert = insert;
 	}
 
+	// GRAPH
 	@Override
 	public String getGraph() {
-		// TODO Auto-generated method stub
-		return null;
+		return graph;
 	}
 
 	@Override
 	public void setGraph(String graph) {
-		// TODO Auto-generated method stub
-
+		this.graph = graph;
 	}
 
+	// USING
 	@Override
 	public String getUsing() {
-		// TODO Auto-generated method stub
-		return null;
+		return using;
 	}
 
 	@Override
 	public void setUsing(String using) {
-		// TODO Auto-generated method stub
+		this.using = using;
+	}
 
+	// WHERE
+	@Override
+	public String getWhere() {
+		return where;
+	}
+
+	@Override
+	public void setWhere(String where) {
+		this.where = where;
+	}
+
+	// GRAPH BASE
+	public String getGraphBase() {
+		return graphbase;
+	}
+
+	/**
+	 * graph base to set the graph to insert into. 
+	 * example: http://glytoucan.org/rdf/demo/0.7
+	 * @param base
+	 */
+	public void setGraphBase(String base) {
+		this.graphbase = base;
 	}
 
 	@Override
 	public String getFormat() {
-		// TODO Auto-generated method stub
-		return null;
+		return InsertSparql.SPARQL;
+	}
+
+	// SPARQL Entity
+	@Override
+	public SparqlEntity getSparqlEntity() {
+		return this.sparqlEntity;
+	}
+
+	@Override
+	public void setSparqlEntity(SparqlEntity sparqlentity) {
+		this.sparql = null;
+		this.sparqlEntity = sparqlentity;
+	}
+
+	@Override
+	public String getSparql() throws SparqlException {
+    	StringBuffer sparqlbuf = new StringBuffer();
+		if (this.sparql == null) {
+			sparqlbuf.append(getPrefix() != null ? getPrefix() : "");
+			if (getFormat().equals(DeleteInsertSparql.SPARQL)) {
+				sparqlbuf.append("DELETE \n");
+				sparqlbuf.append(getGraph() != null ? "{ GRAPH <" + getGraph() + ">\n" : "");
+				sparqlbuf.append("{ " + getDelete() + " }\n");
+				sparqlbuf.append(getGraph() != null ? "}\n" : "");
+				sparqlbuf.append("INSERT \n");
+				sparqlbuf.append(getGraph() != null ? "{ GRAPH <" + getGraph() + ">\n" : "");
+				sparqlbuf.append("{ " + getDelete() + " }\n");
+				sparqlbuf.append(getGraph() != null ? "}\n" : "");
+				sparqlbuf.append(getUsing() != null ? "USING <" + getUsing() + ">\n" : "");
+				sparqlbuf.append(getWhere() != null ? "WHERE " + getWhere() + "\n" : "");
+			} else {
+				sparqlbuf.append(getDelete());
+			}
+			return sparqlbuf.toString();
+		} else {
+			logger.debug(this.sparql.toString());
+			return this.sparql.toString();
+		}
+	}
+
+	@Override
+	public void setSparql(String sparql) {
+		this.sparql = new StringBuffer(sparql);
 	}
 
 }
+
+
