bde41104630f2367964c237b2d285301d406cc49
aoki <aokinobu@gmail.com>
2018-04-20 00:10:58 +0900

    rm sparqlbean and add dependency

--- src/main/java/org/glycoinfo/rdf/utils/SparqlEntityConverter.java
+++
@@ -1,9 +0,0 @@
-package org.glycoinfo.rdf.utils;
-
-import org.glycoinfo.rdf.SparqlException;
-import org.glycoinfo.rdf.dao.SparqlEntity;
-
-public interface SparqlEntityConverter<T> {
-	
-	public T convert(SparqlEntity e) throws SparqlException;
-}
